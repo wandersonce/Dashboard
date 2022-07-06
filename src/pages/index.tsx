b1cf1e70d927b15ad14bd8e4c4234cdd423e6adf
Wanderson Castro <wandersonce1@hotmail.com>
2022-07-05 18:29:40 -0700

    Login Screen with chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,58 @@
+import {Flex, Input, Button, Stack, FormLabel, FormControl} from '@chakra-ui/react';
 
 export default function Home() {
   return (
-  <h1>Dashboard</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor='pink.500'
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input
+              name="password"
+              type="password"
+              focusBorderColor='pink.500'
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: "gray.900"
+              }}
+              size="lg"
+            />
+        </FormControl>    
+        
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg"> Enter</Button>
+      </Flex>
+    </Flex>
   )
 }
