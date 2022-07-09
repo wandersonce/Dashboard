02d5bf2db18d1a319483689fae87c49f41860a0d
Wanderson Castro <wandersonce1@hotmail.com>
2022-07-08 18:30:31 -0700

    Creating Header Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import {Flex, Button, Stack} from '@chakra-ui/react';
 import Input from '../components/Form/input';
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex
       w="100vw"
@@ -19,10 +19,8 @@ export default function Home() {
         flexDirection="column"
       >
         <Stack spacing="4">
-
-        <Input name="email" type="email" label="E-mail" />
-        <Input name="password" type="password" label="Password" />
-        
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg"> Enter</Button>
