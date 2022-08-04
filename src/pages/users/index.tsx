5eee25986044dd9b6b7bf551fcfcabd4a4565ef9
Wanderson Castro <wandersonce1@hotmail.com>
2022-08-03 17:29:48 -0700

    Treating fething data

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,7 +14,20 @@ export default function UserList() {
     const response = await fetch('http:localhost:3000/api/users')
     const data = await response.json()
     
-    return data;
+    const users =  data.users.map(user => {
+      return{
+        id:user.id,
+        name: user.name,
+        email:user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('us-EN', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    });
+
+    return users;
   })
 
 
@@ -70,20 +83,22 @@ export default function UserList() {
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td px={["4","4","6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Wanderson Castro</Text>
-                    <Text fontSize="sm" color="gray.300">wanderson.o.castro@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>April 10, 2022</Td>}
-                
-
-              </Tr>
+              {data.map( user => {
+                return (
+                  <Tr key={user.id}>
+                    <Td px={["4","4","6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">{user.name}</Text>
+                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>{user.createdAt}</Td>}
+                  </Tr>
+                )
+              })}
             </Tbody>
           </Table>
           <Pagination />
