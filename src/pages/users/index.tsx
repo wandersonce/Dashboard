a6864f7ee591f907725c756104465a3e5b77f369
Wanderson Castro <wandersonce1@hotmail.com>
2022-08-04 17:07:14 -0700

    Adding manual Fetching Btn

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
-import { RiAddLine } from 'react-icons/ri'
+import { Box, Button, ButtonSpinner, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
+import { RiAddLine, RiRefreshLine, RiSipLine } from 'react-icons/ri'
 import {useQuery} from '@tanstack/react-query'
 
 import Sidebar from '../../components/Sidebar'
@@ -11,7 +11,7 @@ import { api } from '../../services/api'
 
 
 export default function UserList() {
-  const {data, isLoading, error} = useQuery(['users'], async () => {
+  const {data, isLoading,isFetching, error ,refetch} = useQuery(['users'], async () => {
     const {data} = await api.get('users')
     
     const users =  data.users.map(user => {
@@ -32,6 +32,10 @@ export default function UserList() {
     staleTime: 1000 * 5, // 5 seconds
   })
 
+  const handleClick = () => {
+    // manually refetch
+    refetch();
+  };
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -45,6 +49,7 @@ export default function UserList() {
   return (
     <Box>
       <Header />
+      
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
@@ -52,12 +57,19 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Users
+              
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
-            <Link href="/users/create" passHref>
-              <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
-                Add New
+            <Box>
+              <Button onClick={handleClick} size="sm" bg="pink.400" mr="4">
+                <RiRefreshLine />
               </Button>
-            </Link>
+              <Link href="/users/create" passHref>
+                <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20" />}>
+                  Add New
+                </Button>
+              </Link>
+            </Box>
           </Flex>
           
           { isLoading ? (
