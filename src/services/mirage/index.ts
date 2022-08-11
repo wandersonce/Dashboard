27f0ce4f31ac846c40cf2ce20540962a0033dfd2
Wanderson Castro <wandersonce1@hotmail.com>
2022-08-10 18:01:34 -0700

    Doing math to pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -29,14 +29,25 @@ export function makeServer() {
     },
 
     seeds(server){
-      server.createList('user', 10)
+      server.createList('user', 200)
     },
 
     routes(){
       this.namespace = 'api';
       this.timing = 750;
 
-      this.get('/users');
+      this.get('/users', function(schema, request) {
+        const {page = 1 , per_page=10} = request.queryParams;
+
+        const total = schema.all('user').length;
+
+        //Pagination math
+        const pageStart= (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        //return an object with and array of users -> users slice on page start until the page end
+        const users = schema.all('user').users.slice(pageStart, pageEnd);
+      });
       this.post('/users');
 
       this.namespace = '';
